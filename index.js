a6a9c414e8d674cd504e6cf59bb0ef1d9fcb572f
Diego Alejandro Gómez <diegogopa@unisabana.edu.co>
2025-09-19 07:23:20 -0500

    Arreglos

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+require("dotenv").config();
 const express = require("express"); //Importamos la libreria Express
 const app = express(); //Para guardar la instancia del servidor
 
-const port = 3000; //Definimos el puerto recomendado
+
 
 const students = require("./data"); //Importamos los datos de data.js
+
+app.get('/', (req, res) => {
+    res.send(`Welcom to the homeworck express`);
+});
 
 app.get("/user-info/:id" , (req, res) => { //Defino GET, lo volvemos dinamico y :id puede ser cualquier numero, req objeto con solicitud del cliente, res objeto para responder
     const IdStudent = req.params.id; //Obtiene el valor que puso el cliente
