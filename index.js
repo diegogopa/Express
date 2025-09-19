47c6e53f4bab72819475afc7c71df41d4bdd3196
Diego Alejandro Gómez <diegogopa@unisabana.edu.co>
2025-09-19 10:41:19 -0500

    Arreglo

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express(); //Para guardar la instancia del servidor
 process.env.PORT;
 
 const students = require("./data"); //Importamos los datos de data.js
-
+app.get('/', (req, res) => {
+    res.send("Welcome to the server. This team has ${process.env.MAX_ID} member(s). Take that into account for searching by IDs");
+});
 app.get("/user-info/:id" , (req, res) => { //Defino GET, lo volvemos dinamico y :id puede ser cualquier numero, req objeto con solicitud del cliente, res objeto para responder
     const IdStudent = req.params.id; //Obtiene el valor que puso el cliente
     if (students[IdStudent]) { //Valida existencia del ID
@@ -13,7 +15,6 @@ app.get("/user-info/:id" , (req, res) => { //Defino GET, lo volvemos dinamico y 
           res.status(404).json({error: "Student not found :("});
     }
 });
-
 
 app.listen(process.env.PORT, () => { //Iniciamos el servidor con el puerto
     console.log(`Example app listening on port ${process.env.PORT}`); //Muestro la direccion 
